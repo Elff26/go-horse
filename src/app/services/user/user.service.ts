6a657f8eb2df74480d5962834bc239c5052f8268
lais.farias <lais.farias@INOTE842>
2021-11-22 19:04:31 -0300

    Logica de cadastro de novo usuario

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
@@ -6,9 +7,17 @@ import { Subject } from 'rxjs';
 })
 export class UserService {
   private onAuth = new Subject<boolean>();
+  private onModal = new Subject<string>();
+  private modal:{Title:string,Description:string} = {Title:"",Description:""};
   logged: boolean = false;
+  private description:string="";
+  private title:string="";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getModalUpdated(){
+    return this.onModal.asObservable();
+  }
 
   onLogin() {
     this.logged = true;
@@ -22,11 +31,29 @@ export class UserService {
 
   onLogout() {
     this.logged = false;
-    
+
     this.onAuth.next(this.logged);
   }
 
   getAuthUpdated() {
     return this.onAuth.asObservable();
+  }
+
+  onInsertUser(name:string,surname:string,email:string,password:string,phone:string,cpf:string):void{
+    const formData = new FormData();
+    formData.append('name',name);
+    formData.append('surname',surname);
+    formData.append('email',email);
+    formData.append('password',password);
+    formData.append('phone',phone);
+    formData.append('cpf',cpf);
+
+    this.http.post<any>(`http://localhost:80/go-horse/backend/user.php`, formData).subscribe(response => {
+      this.title = response.Title;
+      this.onModal.next(this.title);
+
+      console.log("Dentro",this.modal.Description);
+    });
+    console.log("Fora",this.modal.Description);
   }
 }
