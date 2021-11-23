abf4c8d16d54a4e6afe9ef0e509366e7700614f2
lais.farias <lais.farias@INOTE842>
2021-11-23 16:59:25 -0300

    Componente de esqueci minha e recuperar minha senha

--- src/app/components/forgot-passsword/forgot-passsword.component.ts
+++ src/app/components/forgot-passsword/forgot-passsword.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailService } from 'src/app/services/user/email.service';
 import { Modal } from '../modal/modal';
 
 @Component({
@@ -8,11 +10,27 @@ import { Modal } from '../modal/modal';
 })
 export class ForgotPassswordComponent implements OnInit {
 
-  constructor() {
+  pageFormForgotPassword: FormGroup = new FormGroup({});
+  email:string="";
+  title:string="";
+  description:string="";
 
+  constructor(private fb: FormBuilder, private emailService: EmailService) {
+    this.createForm();
   }
 
   ngOnInit(): void {
+  }
+
+  createForm(){
+    this.pageFormForgotPassword = this.fb.group({
+      Email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+    });
+  }
+
+  onSubmit(): void{
+    this.onSendEmail();
+    this.pageFormForgotPassword.reset();
   }
 
   onClick(info: Modal){
@@ -20,5 +38,10 @@ export class ForgotPassswordComponent implements OnInit {
     info.description = "Um email para recuperação de senha foi encaminhado para você, verifique sua caixa de entrada ou span!",
     info.textButton = "Okay",
     info.href = "/login"
+  }
+
+  onSendEmail():void{
+    this.email = this.pageFormForgotPassword.controls['Email'].value;
+    this.emailService.onSendEmail(this.email);
   }
 }
---
+++ src/app/components/recover-password/recover-password.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user/user.service';
+import { ConfirmedValidator } from '../login/validatorPassword';
+
+@Component({
+  selector: 'app-recover-password',
+  templateUrl: './recover-password.component.html',
+  styleUrls: ['./recover-password.component.css', '../../app.component.css']
+})
+export class RecoverPasswordComponent implements OnInit {
+
+  pageFormRecoverPassword: FormGroup = new FormGroup({});
+  email:string="";
+  password:string="";
+
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.createForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void{
+    this.onUpdatePassword();
+    this.pageFormRecoverPassword.reset();
+  }
+
+  createForm(){
+    this.pageFormRecoverPassword = this.fb.group({
+      Email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      Password: ['', [Validators.required, Validators.pattern("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$")]],
+      PasswordConfirm: ['', [Validators.required, Validators.pattern("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$")]],
+    },
+    {
+      validator: ConfirmedValidator('Password', 'PasswordConfirm')
+    });
+  }
+
+  onUpdatePassword():void{
+    this.email = this.pageFormRecoverPassword.controls['Email'].value;
+    this.password = this.pageFormRecoverPassword.controls['PasswordConfirm'].value;
+
+    this.userService.onUpdatePassword(this.email,this.password);
+  }
+}
